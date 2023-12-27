809c408052e4fdf2650388507ef8d513b68e2294
cthorpe2 <cthorpe2@uwyo.edu>
2023-12-26 19:53:45 -0700

    initial push

---
+++ phast_photo/src/HelloWorldComponent.js
@@ -0,0 +1,13 @@
+// HelloWorldComponent.js
+
+import React from 'react';
+
+const HelloWorldComponent = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export default HelloWorldComponent;
