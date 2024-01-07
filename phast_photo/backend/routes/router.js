9f1d3d2b8492c5445109d1ae211111625591068d
Buck Harris <BuckyH307@gmail.com>
2024-01-07 11:46:38 -0700

    Added ability to get image metadata

--- phast_photo/backend/routes/router.js
+++ phast_photo/backend/routes/router.js
@@ -1,17 +1,40 @@
-const express = require('express')
+const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' }); // files will be saved to 'uploads' folder
+const fs = require('fs');
+const exifParser = require('exif-parser');
 
-const router = express.Router()
+const upload = multer({ dest: 'uploads/' }); // files will be saved to 'uploads' folder
+const router = express.Router();
 
 // POST endpoint for file uploads
 router.post('/upload', upload.array('photos'), (req, res) => {
-    // req.files contains the uploaded files
-    console.log(req.files);
-  
-    // Process files or save metadata to database, then
-    res.status(200).json({ message: 'Files uploaded successfully' });
-  });
-  
+    const imageInfoArray = [];
 
-module.exports = router
+    req.files.forEach(file => {
+        const buffer = fs.readFileSync(file.path);
+        let metadata = null;
+
+        try {
+            const parser = exifParser.create(buffer);
+            metadata = parser.parse();
+        } catch (error) {
+            console.error('Error parsing EXIF data:', error);
+        }
+
+        imageInfoArray.push({
+            path: file.path,
+            metadata: metadata
+        });
+    });
+
+    imageInfoArray.forEach(item => {
+      console.log('Path:', item.path);
+      console.log('Metadata:', item.metadata);
+      //console.log('Tags:', item.metadata.tags);
+      // log other properties as needed
+    }); // Output the array
+
+    res.status(200).json({ message: 'Files uploaded successfully', data: imageInfoArray });
+});
+
+module.exports = router;
