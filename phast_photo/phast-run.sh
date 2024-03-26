df5e72994e1066420838b2fa87f69f3f1176709f
Mattyjpru <98782573+Mattyjpru@users.noreply.github.com>
2024-03-26 12:24:39 -0600

    added script

---
+++ phast_photo/phast-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+directory1="./backend"
+directory2="./frontend"
+run_npm_start() {
+    cd "$1" || { echo "Error: $1 directory not found"; exit 1; }
+    echo "Running npm start in $(pwd)"
+    npm start
+}
+run_npm_start "$directory1" &
+run_npm_start "$directory2" &
+
+wait./
